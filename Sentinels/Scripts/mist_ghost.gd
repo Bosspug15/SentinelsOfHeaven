4a9140ae74113be387fc08113bbd9b0483701c80
Bosspug <eugeniojlopezt@gmail.com>
2024-12-07 18:47:13 -0500

    experiments gone wrong

--- Sentinels/Scripts/mist_ghost.gd
+++ Sentinels/Scripts/mist_ghost.gd
@@ -1,13 +1,26 @@
-extends Area2D
+extends Node2D
 
-@onready var timer = $Timer
+@onready var timer = $Lifetime
 
-func _on_body_entered(_body):
-	if _body.is_in_group():
-		_body.die()
-		timer.start()
+var inRange = false
+var vel = 10
+var playerPos
 
+func _physics_process(delta):
+	position = position.move_toward(playerPos.position, delta*vel)
 
 func _on_timer_timeout():
 	queue_free()
 	
+
+
+func _on_test_tracking_body_entered(_body):
+	playerPos = _body.position
+	timer.start()
+	if _body.is_in_group("ghostTrack"):
+		inRange = true
+		
+
+
+func _on_test_tracking_body_exited(_body):
+	inRange = false
