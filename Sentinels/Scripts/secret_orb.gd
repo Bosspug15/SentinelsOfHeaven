0a9fa8fe4154732c5454cdc7de5a667db9c62827
Bosspug <eugeniojlopezt@gmail.com>
2025-07-03 15:44:12 -0400

    cutscene test

--- Sentinels/Scripts/secret_orb.gd
+++ Sentinels/Scripts/secret_orb.gd
@@ -8,7 +8,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		animated_sprite.play("Collected")
-		
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
