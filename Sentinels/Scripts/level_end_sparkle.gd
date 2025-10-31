6b5e134a568bd5a15941bbc2403dc1884eade910
Bosspug <eugeniojlopezt@gmail.com>
2025-10-30 23:26:15 -0400

    particles and some clean up and some bg tests

--- Sentinels/Scripts/level_end_sparkle.gd
+++ Sentinels/Scripts/level_end_sparkle.gd
@@ -4,12 +4,14 @@ extends Area2D
 @export var fade_to_white : AnimationPlayer
 @onready var timer = $timer
 @onready var fade_timer = $FadeTimer
+@onready var sparkle_sprite = $Sparkle
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player.fade_sprite.play("FadeInTest")
 		fade_to_white.play("FadeToWhite")
+		sparkle_sprite.play("Flash")
 		fade_timer.start()
 		player.playerLock()
 
@@ -18,3 +20,7 @@ func _on_transition_timer_timeout() -> void:
 
 func _on_fade_timer_timeout() -> void:
 	fade_to_white.play("ShowItem")
+
+func _on_sparkle_animation_finished() -> void:
+	if sparkle_sprite.animation == "Flash":
+		self.visible = false
